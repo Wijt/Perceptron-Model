c0191c67846e6f71f7eb521859e4c5f19b5d54ea
Furkan Kaya <8fkaya@gmail.com>
2020-06-06 12:25:43 +0300

    Separator added.

---
+++ data/separator.js
@@ -0,0 +1,17 @@
+class Separator {
+    constructor(func, size, color) {
+        this.first = {x: min.x, y: func(min.x)};
+        this.second = {x: max.x, y: func(max.x)};
+        this.color = color;
+        this.size = size;
+        separators.push(this);
+    }
+
+    show(){
+        push();
+            strokeWeight(this.size)
+            stroke(this.color);
+            line(this.first.x, this.first.y, this.second.x, this.second.y);
+        pop();
+    }
+}
